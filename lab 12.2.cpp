2942f7d406174cbd76571bea8fbba3f1bd13206e
Ruslan172005 <146805874+Ruslan172005@users.noreply.github.com>
2023-11-23 21:56:33 +0200

    Update lab 12.2.cpp

--- lab 12.2.cpp
+++ lab 12.2.cpp
@@ -1,30 +1,25 @@
-#include <iostream>
-#include <cmath>
-
-int main() {
-    // Виберемо варіант
-    int i = 7;
-
-    // Обчислимо a
-    double a = 0;
-    for (int k = i + 5; k <= i + 13; ++k) {
-        a += fabs(fabs(sin(2 * k - 1.5)) + 3 * sin(4 * k)) + 2.38;
-    }
-
-    // Обчислимо b
-    double b = 1;
-    for (int k = i; k <= i + 8; ++k) {
-        b *= cos(k / 2.1 + cos(k) / 1.1 - 8.3 * sin(3 * k + 1));
-    }
-
-    // Обчислимо z
-    double z = pow(a, b);
-
-    // Виведемо значення i, a, b, z
-    std::cout << "i: " << i << std::endl;
-    std::cout << "a: " << a << std::endl;
-    std::cout << "b: " << b << std::endl;
-    std::cout << "z: " << z << std::endl;
-
-    return 0;
+#include <iostream> 
+#include <cmath> 
+using namespace std; 
+ 
+int main() 
+{ 
+    double z, a = 0; 
+    double b = 1; 
+    int i = 7; 
+    int k = i; 
+    double x = i; 
+    
+ 
+    do { 
+        a += cos(pow(x, 3)) / 2.1 + cos(pow(x, 2)) / 1.1 - 8.3 * sin(3 * x + 1);
+        k++; 
+    } while (k < i + 5); 
+    do { 
+        b *= sin(pow(x, 2)) * cos(pow(x, 3)) - sin(x) + 5.2;
+        k++; 
+    } while (k < i + 8); 
+    z = pow(a , b); 
+    cout << "i = " << i << " a = " << a << " b = " << b << " z = " << int(z) << endl; 
+    return 0; 
 }
