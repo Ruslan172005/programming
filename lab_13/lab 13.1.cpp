208e429690a81d3d4015e0a924637599cfadb1eb
Ruslan172005 <146805874+Ruslan172005@users.noreply.github.com>
2023-12-06 17:01:46 +0200

    Update lab 13.1.cpp

--- lab_13/lab 13.1.cpp
+++ lab_13/lab 13.1.cpp
@@ -1,22 +1,36 @@
 #include <iostream>
-#include <cmath>
 #include <iomanip>
+#include <cmath>
 
 int main() {
-    const double i = 7.0;
-    const double h = 0.7;
-    const double epsilon = 1e-10;
+    setlocale(LC_ALL, "UKR");
+    double start = 0.0;
+    double end = 1.0;
+    double step = 0.1;
+    double x, y, sumPositive = 0.0;
+    int countNegative = 0;
 
-    std::cout << std::fixed << std::setprecision(2);
-    std::cout << "x\t\t y\n";
-    std::cout << "---------------------\n";
+    std::cout << std::setw(10) << "x" << std::setw(15) << "y" << std::endl;
+    std::cout << "---------------------" << std::endl;
 
-    double x = 0.0;
-    while (x <= i + epsilon) {
-        double y = std::abs(std::cos(x) * std::cos(x) - 0.51) * std::sin(3 * x - 4) - 4.44;
-        std::cout << x << "\t\t" << y << "\n";
-        x += h;
+    for (x = start; x <= end; x += step) {
+        y = std::fabs(std::cos(x * x - 0.51) * std::sin(3 * x - 4) - 4.44);
+
+        std::cout << std::fixed << std::setprecision(2);
+        std::cout << std::setw(10) << x << std::setw(15) << y << std::endl;
+
+        // Знаходження суми додатних значень та кількості від'ємних
+        if (y > 0) {
+            sumPositive += y;
+        }
+        else {
+            countNegative++;
+        }
     }
+
+    std::cout << "---------------------" << std::endl;
+    std::cout << "Сума додатних значень: " << sumPositive << std::endl;
+    std::cout << "Кількість від'ємних значень: " << countNegative << std::endl;
 
     return 0;
 }
